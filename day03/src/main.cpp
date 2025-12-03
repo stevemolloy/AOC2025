facd6954322536be8f2dd811b3d7e9a589de8cfc
Stephen Molloy <sdmolloy@gmail.com>
2025-12-03 23:24:12 +0100

    Bikeshedding

--- day03/src/main.cpp
+++ day03/src/main.cpp
@@ -5,12 +5,14 @@
 #include <fstream>
 #include <string>
 
+using std::println;
+using std::max_element;
+
 constexpr int BATT_PART2 =  12;
 
 int main(void) {
-    std::string data_dir = "data/";
-    // std::ifstream input_data(data_dir + "test.txt");
-    std::ifstream input_data(data_dir + "input.txt");
+    // std::ifstream input_data("data/test.txt");
+    std::ifstream input_data("data/input.txt");
 
     unsigned long int part1 = 0;
     unsigned long int part2 = 0;
@@ -23,20 +25,20 @@ int main(void) {
             vals.push_back(c - '0');
         }
 
-        auto b1 = std::max_element(vals.begin(), vals.end()-1);
-        auto b2 = std::max_element(b1+1, vals.end()-0);
+        auto b1 = max_element(vals.begin(), vals.end()-1);
+        auto b2 = max_element(b1+1, vals.end()-0);
         part1 += *b1*10 + *b2;
 
         auto prev = vals.begin();
         for (int i=BATT_PART2-1; i>=0; i--) {
-            auto largest = std::max_element(prev, vals.end()-i);
+            auto largest = max_element(prev, vals.end()-i);
             prev = largest + 1;
             part2 += *largest * std::pow(10, i);
         }
     }
 
-    std::println("Part 1: {}", part1);
-    std::println("Part 2: {}", part2);
+    println("Part 1: {}", part1);
+    println("Part 2: {}", part2);
 
     return 0;
 }

d6c58b169278713cf78cf279e6268cd18159dbc3
Stephen Molloy <sdmolloy@gmail.com>
2025-12-06 22:19:39 +0100

    Refactor

--- day06/src/main.cpp
+++ day06/src/main.cpp
@@ -11,70 +11,25 @@ using std::vector;
 
 using BinFunc = std::function<ulong(ulong, ulong)>;
 
-std::string get_last_line(std::ifstream& in) {
-    std::string line;
-    while (in >> std::ws && std::getline(in, line));
-    return line;
-}
+typedef struct {
+    vector<vector<ulong>> vals;
+    std::vector<char> functors;
+} ParsedData;
+
+ParsedData parse_data_part1(std::string filename);
+ulong solve_part1(ParsedData parsed_data);
+std::string get_last_line(std::ifstream& in);
 
 int main(void) {
     // std::string filename = "data/test.txt";
     // std::string filename = "data/test2.txt";
     std::string filename = "data/input.txt";
 
-    std::ifstream input_data(filename);
-
-    std::string data_line;
-    vector<vector<ulong>> vals;
-    std::vector<char> functors;
-    bool first_iter = true;
-    while (std::getline(input_data, data_line)) {
-        size_t counter = 0;
-        ulong val;
-
-        size_t start=0, end=0;
-        char dl = ' ';
-        while ((start = data_line.find_first_not_of(dl, end)) != std::string::npos) {
-            end = data_line.find(dl, start);
-
-            if (data_line[start] != '*' && data_line[start] != '+') {
-                val = (ulong)std::atoi(data_line.substr(start, end - start).c_str());
-            } else if (data_line[start] == '+') {
-                functors.push_back('+');
-                continue;
-            } else if (data_line[start] == '*') {
-                functors.push_back('*');
-                continue;
-            } else {
-                println("ERROR: Unknown character '{}'", data_line[start]);
-                exit(1);
-            }
-
-            if (first_iter) {
-                vector<ulong> col;
-                col.push_back(val);
-                vals.push_back(std::move(col));
-            } else {
-                vals[counter].push_back(val);
-                counter += 1;
-            }
-        }
-
-        first_iter = false;
-    }
-    input_data.close();
-
-    ulong part1 = 0;
-    for (size_t i=0; i<functors.size(); i++) {
-        ulong result;
-        if (functors[i] == '+') result = std::ranges::fold_left(vals[i], 0, std::plus<ulong>());
-        else if (functors[i] == '*') result = std::ranges::fold_left(vals[i], 1, std::multiplies<ulong>());
-        part1 += result;
-    }
-
+    ParsedData parsed_data = parse_data_part1(filename);
+    ulong part1 = solve_part1(parsed_data);
     if (filename == "data/input.txt") assert(part1 == 6169101504608);
 
-    input_data.open(filename);
+    std::ifstream input_data(filename);
     std::string last_line = get_last_line(input_data);
     std::reverse(last_line.begin(), last_line.end());
     input_data.close();
@@ -94,6 +49,7 @@ int main(void) {
     }
 
     input_data.open(filename);
+    std::string data_line;
     vector<vector<ulong>> part2digits;
     while (std::getline(input_data, data_line)) {
         std::reverse(data_line.begin(), data_line.end());
@@ -117,7 +73,7 @@ int main(void) {
         }
         part2vals.push_back(val);
     }
-    std::reverse(functors.begin(), functors.end());
+    std::reverse(parsed_data.functors.begin(), parsed_data.functors.end());
 
     ulong part2 = 0;
     size_t index = 0;
@@ -125,8 +81,8 @@ int main(void) {
         part2 += std::ranges::fold_left(
                 part2vals.begin()+index,
                 part2vals.begin()+index+lengths[i],
-                functors[i]=='+' ? 0 : 1,
-                functors[i]=='+' ? (BinFunc)std::plus<ulong>() : (BinFunc)std::multiplies<ulong>()
+                parsed_data.functors[i]=='+' ? 0 : 1,
+                parsed_data.functors[i]=='+' ? (BinFunc)std::plus<ulong>() : (BinFunc)std::multiplies<ulong>()
             );
         index += lengths[i] + 1;
     }
@@ -138,4 +94,66 @@ int main(void) {
 
     return 0;
 }
+
+ParsedData parse_data_part1(std::string filename) {
+    ParsedData result;
+    std::ifstream input_data(filename);
+    std::string data_line;
+    bool first_iter = true;
+    while (std::getline(input_data, data_line)) {
+        size_t counter = 0;
+        ulong val;
+
+        size_t start=0, end=0;
+        char dl = ' ';
+        while ((start = data_line.find_first_not_of(dl, end)) != std::string::npos) {
+            end = data_line.find(dl, start);
+
+            if (data_line[start] != '*' && data_line[start] != '+') {
+                val = (ulong)std::atoi(data_line.substr(start, end - start).c_str());
+            } else if (data_line[start] == '+') {
+                result.functors.push_back('+');
+                continue;
+            } else if (data_line[start] == '*') {
+                result.functors.push_back('*');
+                continue;
+            } else {
+                println("ERROR: Unknown character '{}'", data_line[start]);
+                exit(1);
+            }
+
+            if (first_iter) {
+                vector<ulong> col;
+                col.push_back(val);
+                result.vals.push_back(std::move(col));
+            } else {
+                result.vals[counter].push_back(val);
+                counter += 1;
+            }
+        }
+
+        first_iter = false;
+    }
+    input_data.close();
+
+    return result;
+}
+
+ulong solve_part1(ParsedData parsed_data) {
+    ulong res = 0;
+    for (size_t i=0; i<parsed_data.functors.size(); i++) {
+        ulong result;
+        if (parsed_data.functors[i] == '+') result = std::ranges::fold_left(parsed_data.vals[i], 0, std::plus<ulong>());
+        else if (parsed_data.functors[i] == '*') result = std::ranges::fold_left(parsed_data.vals[i], 1, std::multiplies<ulong>());
+        res += result;
+    }
+    return res;
+}
+
+std::string get_last_line(std::ifstream& in) {
+    std::string line;
+    while (in >> std::ws && std::getline(in, line));
+    return line;
+}
+
 

3a6fe65882917e5d28a79d462fe6757b1438f38d
Stephen Molloy <sdmolloy@gmail.com>
2025-12-07 17:10:59 +0100

    Lots of functions

--- day07/src/main.cpp
+++ day07/src/main.cpp
@@ -1,7 +1,5 @@
-#include <algorithm>
 #include <cassert>
 #include <cstdlib>
-#include <functional>
 #include <ranges>
 #include <string>
 #include <vector>
@@ -15,6 +13,9 @@ using std::vector;
 vector<std::string> read_file(std::string filename);
 void print_num_grid(vector<vector<long>> num_grid);
 size_t solve_part1(vector<std::string> &working_grid);
+long sum_paths(vector<vector<long>> &num_grid);
+vector<vector<long>> make_num_grid(vector<std::string> &working_grid);
+void draw_paths(vector<vector<long>> &num_grid);
 
 int main(void) {
     // std::string filename = "data/test.txt";
@@ -26,39 +27,9 @@ int main(void) {
     size_t part1 = solve_part1(working_grid);
     if (filename == "data/input.txt") assert(part1 == 1717);
 
-    vector<vector<long>> num_grid;
-    for (auto row: working_grid) {
-        vector<long> num_row;
-        num_row.push_back(0);
-        for (char c: row) {
-            if (c=='^') num_row.push_back(-1);
-            else if (c=='S') num_row.push_back(1);
-            else num_row.push_back(0);
-        }
-        num_row.push_back(0);
-        num_grid.push_back(num_row);
-    }
-
-    for (size_t row_ind=1; row_ind<num_grid.size(); row_ind++) {
-        vector<long> this_row = num_grid[row_ind];
-        vector<long> prev_row = num_grid[row_ind-1];
-        for (size_t i=0; i<this_row.size(); i++) {
-            if (prev_row[i] > 0 && this_row[i] >= 0) {
-                num_grid[row_ind][i] += prev_row[i];
-            } else if (prev_row[i] > 0 && this_row[i]<0) {
-                num_grid[row_ind][i-1] += prev_row[i];
-                num_grid[row_ind][i+1] += prev_row[i];
-            }
-        }
-    }
-
-    long part2 = 0;
-    vector<long> last_row = num_grid.back();
-    for (size_t i=0; i<last_row.size(); i++) {
-        long val = last_row[i];
-        part2 += val>=0 ? val : 0;
-    }
-
+    vector<vector<long>> num_grid = make_num_grid(working_grid);
+    draw_paths(num_grid);
+    long part2 = sum_paths(num_grid);
     if (filename == "data/input.txt") assert(part2 == 231507396180012);
 
     println("Part 1: {}", part1);
@@ -106,5 +77,44 @@ size_t solve_part1(vector<std::string> &working_grid) {
         }
     }
     return part1;
+}
+
+long sum_paths(vector<vector<long>> &num_grid) {
+    long part2 = 0;
+    vector<long> last_row = num_grid.back();
+    for (size_t i=0; i<last_row.size(); i++) {
+        long val = last_row[i];
+        part2 += val>=0 ? val : 0;
+    }
+    return part2;
+}
+
+vector<vector<long>> make_num_grid(vector<std::string> &working_grid) {
+    vector<vector<long>> num_grid;
+    for (auto row: working_grid) {
+        vector<long> num_row;
+        for (char c: row) {
+            if (c=='^')      num_row.push_back(-1);
+            else if (c=='S') num_row.push_back(1);
+            else             num_row.push_back(0);
+        }
+        num_grid.push_back(num_row);
+    }
+    return num_grid;
+}
+
+void draw_paths(vector<vector<long>> &num_grid) {
+    size_t max_row_ind = num_grid.size();
+    for (size_t row_ind=1; row_ind<max_row_ind; row_ind++) {
+        size_t max_i = num_grid[row_ind].size();
+        for (size_t i=0; i<max_i; i++) {
+            if (num_grid[row_ind-1][i] > 0 && num_grid[row_ind][i] >= 0) {
+                num_grid[row_ind][i] += num_grid[row_ind-1][i];
+            } else if (num_grid[row_ind-1][i] > 0 && num_grid[row_ind][i]<0) {
+                num_grid[row_ind][i-1] += num_grid[row_ind-1][i];
+                num_grid[row_ind][i+1] += num_grid[row_ind-1][i];
+            }
+        }
+    }
 }
 

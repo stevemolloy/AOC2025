0a9c56ca8bbad73d251288c7f1d06867fb84e100
Stephen Molloy <sdmolloy@gmail.com>
2025-12-07 18:04:18 +0100

    Constrefs for speed

--- day07/src/main.cpp
+++ day07/src/main.cpp
@@ -10,11 +10,11 @@ using std::println;
 using std::print;
 using std::vector;
 
-vector<std::string> read_file(std::string filename);
-void print_num_grid(vector<vector<long>> num_grid);
+vector<std::string> read_file(const std::string &filename);
+void print_num_grid(const vector<vector<long>> &num_grid);
 size_t solve_part1(vector<std::string> &working_grid);
-long sum_paths(vector<vector<long>> &num_grid);
-vector<vector<long>> make_num_grid(vector<std::string> &working_grid);
+long sum_paths(const vector<vector<long>> &num_grid);
+vector<vector<long>> make_num_grid(const vector<std::string> &working_grid);
 void draw_paths(vector<vector<long>> &num_grid);
 
 int main(void) {
@@ -38,16 +38,16 @@ int main(void) {
     return 0;
 }
 
-vector<std::string> read_file(std::string filename) {
+vector<std::string> read_file(const std::string &filename) {
     std::ifstream file_stream(filename);
     vector<std::string> file_contents;
     std::string line;
     while (std::getline(file_stream, line))
-        file_contents.push_back(line);
+        file_contents.push_back(std::move(line));
     return file_contents;
 }
 
-void print_num_grid(vector<vector<long>> num_grid) {
+void print_num_grid(const vector<vector<long>> &num_grid) {
     println("------------------------------");
     for (auto row: num_grid) {
         for (long v: row) {
@@ -63,23 +63,27 @@ void print_num_grid(vector<vector<long>> num_grid) {
 
 size_t solve_part1(vector<std::string> &working_grid) {
     size_t part1 = 0;
-    for (size_t i=1; i<working_grid.size(); i++) {
-        for (const auto [j, c]: std::ranges::views::enumerate(working_grid[i])) {
-            if (c=='.' && working_grid[i-1][j]=='S') {
-                working_grid[i][j] = '|';
-            } else if (c=='^' && working_grid[i-1][j]=='|') {
-                if ((int)j>0) working_grid[i][j-1] = '|';
-                if ((int)j<(int)working_grid[i].size()-1) working_grid[i][j+1] = '|';
+    size_t max_i = working_grid.size();
+    for (size_t i=1; i<max_i; i++) {
+        const auto& prev_row = working_grid[i-1];
+        auto& curr_row = working_grid[i];
+        for (const auto [j, c]: std::ranges::views::enumerate(curr_row)) {
+            char p = prev_row[j];
+            if (c=='.' && p=='S') {
+                curr_row[j] = '|';
+            } else if (c=='^' && p=='|') {
+                if ((int)j>0) curr_row[j-1] = '|';
+                if ((int)j<(int)curr_row.size()-1) curr_row[j+1] = '|';
                 part1 += 1;
-            } else if (c=='.' && working_grid[i-1][j]=='|') {
-                working_grid[i][j] = '|';
+            } else if (c=='.' && p=='|') {
+                curr_row[j] = '|';
             }
         }
     }
     return part1;
 }
 
-long sum_paths(vector<vector<long>> &num_grid) {
+long sum_paths(const vector<vector<long>> &num_grid) {
     long part2 = 0;
     vector<long> last_row = num_grid.back();
     for (size_t i=0; i<last_row.size(); i++) {
@@ -89,7 +93,7 @@ long sum_paths(vector<vector<long>> &num_grid) {
     return part2;
 }
 
-vector<vector<long>> make_num_grid(vector<std::string> &working_grid) {
+vector<vector<long>> make_num_grid(const vector<std::string> &working_grid) {
     vector<vector<long>> num_grid;
     for (auto row: working_grid) {
         vector<long> num_row;
@@ -98,7 +102,7 @@ vector<vector<long>> make_num_grid(vector<std::string> &working_grid) {
             else if (c=='S') num_row.push_back(1);
             else             num_row.push_back(0);
         }
-        num_grid.push_back(num_row);
+        num_grid.push_back(std::move(num_row));
     }
     return num_grid;
 }
